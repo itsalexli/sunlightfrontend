4bf3e5d916d8f3f788f232c7180de3134f1a8eaf
ye-allison <allison.ye111@gmail.com>
2025-01-26 10:22:24 -0500

    changes

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,15 +1,16 @@
-export default function Button({ name, state, width, onClick, content }) {
-  const isActive = state === name.toLowerCase();
+export default function Button({ name, state, width, onClick }) {
+  const isActive = state === name.toLowerCase(); // Check if button is active
+
   const buttonStyle = isActive
-    ? "bg-[#013946] text-white font-bold"
-    : "bg-[#E6ECED] text-[#013946] font-bold";
+    ? "bg-[#013946] text-white font-semibold outline outline-2 outline-[#013946]"
+    : "bg-[#E6ECED] outline outline-2 outline-[#013946] text-[#013946] font-semibold hover:bg-[#436E95]";
 
   return (
     <button
-      onClick={onClick}
-      className={`mr-4 w-${width || "auto"} px-6 py-3 ${buttonStyle} rounded`}
+      onClick={onClick} // Enable tab switching
+      className={`mr-4 w-${width || "auto"} px-6 py-2 rounded ${buttonStyle}`}
     >
-      {content}
+      {name}
     </button>
   );
 }
